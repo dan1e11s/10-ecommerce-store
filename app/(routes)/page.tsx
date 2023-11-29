f76c2d552f24030647591aa106152cd9eb951969
Daniel <danielmuratov2003@gmail.com>
2023-11-29 18:18:53 +0600

    added billboards action

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,11 +1,17 @@
 import Billboard from '@/components/Billboard';
 import Container from '@/components/ui/Container';
 
-const HomePage = () => {
+import getBillboards from '@/actions/get-billboards';
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboards('13017d6d-1a60-4d9f-98d1-1d6bd1de97e6');
+
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard />
+        <Billboard data={billboard} />
       </div>
     </Container>
   );
