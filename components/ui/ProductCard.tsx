b6be8756a5194bd92010f3f2bf3d02d5537832c7
Daniel <danielmuratov2003@gmail.com>
2023-12-01 22:29:26 +0600

    add handleClick

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -6,16 +6,24 @@ import { Expand, ShoppingCart } from 'lucide-react';
 import { Product } from '@/types';
 import IconButton from './IconButton';
 import Currency from './Currency';
+import { useRouter } from 'next/router';
 
 interface ProductCardProps {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
-  console.log(data);
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/products/${data.id}`);
+  };
 
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
           src={data?.images?.[0]?.url}
